e8aa5f67d7d8c5baaec7411a6ccf1985af709dcd
AlvinOng1999 <alvinongyeehung@gmail.com>
2023-05-31 08:55:10 +0000

    make changes to fix ero_r

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("The last change for today", app.getMessage());
+        assertEquals("I love Jenkins", app.getMessage());
     }
 }
