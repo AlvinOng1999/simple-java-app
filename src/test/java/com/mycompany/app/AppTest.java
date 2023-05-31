9368ad962307ba6e6875b75d85f64de1f82f5f53
AlvinOng1999 <alvinongyeehung@gmail.com>
2023-05-31 08:51:11 +0000

    Some changes to test webhooks

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("I love Jenkins", app.getMessage());
+        assertEquals("The last change for today", app.getMessage());
     }
 }
