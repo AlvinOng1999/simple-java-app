e05c4020a4eaebaf3b88995f6ce78ff89cf0781e
AlvinOng1999 <alvinongyeehung@gmail.com>
2023-06-01 05:33:35 +0000

    My XXXXX change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("The last changes of today");
+        System.out.println("changes");
     }
 
     public String getMessage() {
